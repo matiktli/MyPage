3089936daf660d3a7d11b32399d3f4efe27fd463
matikitli <m.kitlinos@gmail.com>
2019-09-17 23:35:05 +0100

    clean

--- src/app/service/vschelper/vsc-helper.service.ts
+++ src/app/service/vschelper/vsc-helper.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VscHelperService {
 
-  constructor() {
-    
-   }
+  constructor(private http: HttpClient) { }
 
-  
-  
+
+  getCommitsByUser(): Array<String> {
+    return
+  }
+
+
 }
