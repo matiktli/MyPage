b4c099705fd626b912ca6e9217b239f8faea63cd
matikitli <m.kitlinos@gmail.com>
2019-09-18 00:56:29 +0100

    Slow progress reading through docs

---
+++ src/app/component/git-info/git-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-info',
+  templateUrl: './git-info.component.html',
+  styleUrls: ['./git-info.component.css']
+})
+export class GitInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
